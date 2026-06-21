c894834a6e84c8508fb9f651d40d29f678dde38d
Jadx <prajadisa2025@gmail.com>
2026-06-21 17:21:05 +0700

    Correct header text from 'Kotak' to 'Kontak'

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,7 @@ function App() {
 
                 {/*Kontak*/}
                 <div className="kontak mt-32 sm:p-10 p-0" id="kontak">
-                    <h1 className="text-4xl mb-2 font-bold text-center" data-aos="fade-up" data-aos-duration="1000">Kotak</h1>
+                    <h1 className="text-4xl mb-2 font-bold text-center" data-aos="fade-up" data-aos-duration="1000">Kontak</h1>
                     <p className="text-base/loose text-center mb-10 opacity-50" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">Mari terhubung dengan saya.</p>
                     <form action="https://formsubmit.co/prajadisa2025@gmail.com" method="POST" className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md" autoComplete="off" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
                         <div className="flex flex-col gap-6">
